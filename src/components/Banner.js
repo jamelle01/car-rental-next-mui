eed78a2b615ce952b5dd240c263b34e6491a138e
Ell do <91648854+jamelle01@users.noreply.github.com>
2023-06-06 22:03:03 +0800

    add banner

---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import { Box, Typography } from "@mui/material";
+
+const Banner = () => {
+  return (
+    <Box>
+      <Typography variant="h1">Save big with our cheap car rental!</Typography>
+      <Typography variant="h4">
+        Top Airports. Local Suppliers. 24/7 Support.
+      </Typography>
+    </Box>
+  );
+};
+
+export default Banner;

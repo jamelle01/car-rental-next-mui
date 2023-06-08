bb9521a2f5f3d30c36d88b5489d584f694c270a0
Ell do <91648854+jamelle01@users.noreply.github.com>
2023-06-09 07:56:09 +0800

    attempt deployment debug

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import Footer from "@/components/Footer";
+import Footer from "@/components/Footersss";
 
 export default function App({ Component, pageProps }) {
   return (

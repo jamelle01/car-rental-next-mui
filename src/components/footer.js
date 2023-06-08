2428b8704c4e7088977a39bded4467dcf76de302
Ell do <91648854+jamelle01@users.noreply.github.com>
2023-06-08 10:57:26 +0800

    footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,19 @@
-import { Box } from "@mui/icons-material";
+import { Grid } from "@mui/icons-material";
+import { Typography } from "@mui/material";
 
 const Footer = () => {
   return (
     <div>
-      <Box></Box>
+      <Grid container>
+        <Grid xs={3}>
+          <Typography variant="h3">CAR Rental</Typography>
+          <Typography variant="subtitle2" color={"lightgray"}>
+            We offers a big range of vehicles for all your driving needs. We
+            have the perfect car to meet your needs.
+          </Typography>
+          (123) -456-789 carrental@gmail.com
+        </Grid>
+      </Grid>
     </div>
   );
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,16 +2,18 @@ import ResponsiveAppBar from "@/components/Navbar";
 import "@/styles/globals.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/styles/theme";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <ResponsiveAppBar />
       <Component {...pageProps} />
+      <Footer/>
     </ThemeProvider>
   );
 }

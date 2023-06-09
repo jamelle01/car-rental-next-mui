14b160bf0f2a8db7689345c471b26c7bb7ea41e3
Ell do <91648854+jamelle01@users.noreply.github.com>
2023-06-09 08:05:10 +0800

    rename footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,7 +6,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import Footer from "@/components/Footersss";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }) {
   return (

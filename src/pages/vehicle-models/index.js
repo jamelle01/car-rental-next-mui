9aa5f0041dee68f211c00c9ad3ca7449ae7a8a44
Ell do <91648854+jamelle01@users.noreply.github.com>
2023-07-10 21:39:39 +0800

    add vehicle models card

--- src/pages/vehicle-models/index.js
+++ src/pages/vehicle-models/index.js
@@ -1,6 +1,15 @@
 import HeroPages from "@/components/HeroPages";
 import Banner from "@/components/about/Banner";
-import { Container } from "@mui/material";
+import {
+  Container,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
 import Head from "next/head";
 
 const VehicleModels = () => {
@@ -10,6 +19,52 @@ const VehicleModels = () => {
         <title>Car Rental | Vehicle Models</title>
       </Head>
       <HeroPages name="Vehicle Models" />
+      <Grid container spacing={3}>
+        <Grid item xs={4}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="280"
+              image="/images/cars-big/audi-box.png"
+            />
+            <CardContent>
+              <Grid container sx={{background: 'red'}}>
+                <Grid item>
+                  <Typography
+                    gutterBottom
+                    variant="h4"
+                    fontWeight={"bold"}
+                    component="div"
+                  >
+                    Lizard
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography
+                    // gutterBottom
+                    variant="h4"
+                    fontWeight={"bold"}
+                    // component="div"
+                    sx={{ textAlign: "right" }}
+                  >
+                    $45
+                  </Typography>
+                </Grid>
+              </Grid>
+
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
       <Banner />
     </Container>
   );
